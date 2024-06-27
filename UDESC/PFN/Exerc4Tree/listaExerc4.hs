f32ebdb382d69f488a967a799468167e1024340f
LucianoKohler <108994222+LucianoKohler@users.noreply.github.com>
2024-06-27 16:33:02 -0300

    Update listaExerc4.hs

--- UDESC/PFN/Exerc4Tree/listaExerc4.hs
+++ UDESC/PFN/Exerc4Tree/listaExerc4.hs
@@ -7,7 +7,7 @@ type Line = String
 type Word' = String
 type Index = Int
 
-data Arv a = No a (Arv a) (Arv a) | Nil deriving (Show)
+data Arv = Node Word' [Index] Arv Arv | Nil deriving (Show)
 
 -- A já está resolvida com o comando Lines
 
@@ -15,50 +15,49 @@ data Arv a = No a (Arv a) (Arv a) | Nil deriving (Show)
 -- lines x = lines x
 
 -- B Numerar as linhas com um index
--- Input teste: numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional"
 
-numLines :: Doc -> [(Index, Line)]
-numLines str = numLinesAux 1 (lines str)
-
-numLinesAux :: Index -> [Line] -> [(Index, Line)]
-numLinesAux _ [] = []
-numLinesAux num (linha : linhas) = (num, linha) : numLinesAux (num + 1) linhas
-
-numLinesElegante xs = zip [1 ..] (lines xs)
+numLines xs = zip [1 ..] (lines xs)
 
 -- C apontar em que linha cada palavra aparece
--- Input teste: allNumWords (numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional")
 
 allNumWords :: [(Index, Line)] -> [(Index, Word')]
 allNumWords [] = []
 allNumWords ((index, linha) : xs) = zip (repeat index) (words linha) ++ allNumWords xs
 
 -- D Ordenar alfabeticamente as ocorrências de palavras no texto
--- Input teste: sortLs (allNumWords (numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional"))
+-- E Juntar as ocorrências de cada palavra, para cada palavra
 
-insTree e Nil = No e Nil Nil
-insTree e (No x esq dir) | e == x = No x esq dir
-                         | e < x   = No x (insTree e esq) dir
-                         | e > x   = No x esq (insTree e dir)
+listToTree [] =  Nil
+listToTree ((ind, pal):xs) = insTree pal ind (listToTree xs)
 
-sortLs [] = Nil
-sortLs (x:xs) = insTree x (sortLs xs)
+insTree _ _ [] = Nil Nil
+insTree p i (Node pal li esq dir) | p == pal = Node p (insOrd i li) esq dir
+                                  | p < pal  = Node p (insTree e esq) dir
+                                  | p > pal  = Node p esq (insTree e dir)
 
--- E Juntar as ocorrências de cada palavra, para cada palavra, mo	
--- Input teste: almalgamate (sortLs (allNumWords (numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional")))
-
-amalgamate :: Arv (Index, Word') -> Arv ([Index], Word')
-amalgamate Nil = Nil
-amalgamate (No (index, word) esq dir) = -- À fazer
-
--- F Eliminar da lista de números de linhas em que cada palavra ocorre, as repetições de um mesmo número de linha:
--- Input teste: shorten (almalgamate (sortLs (allNumWords (numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional"))))  
-
--- Função para imprimir árvore:
+toLowercase :: String -> String
+toLowercase [] = []
+toLowercase (c : xs)
+  | c >= 'A' && c <= 'Z' = toEnum (fromEnum c + 32) : toLowercase xs
+  | otherwise = c : toLowercase xs
 
 arvPrint Nil = return []
-arvPrint (No x esq dir) = do arvPrint esq
-                             putStr (show x ++ "\n")
-                             arvPrint dir
+arvPrint (Node pal li esq dir) = do arvPrint esq
+                                    putStr (show pal ++ ", " ++ show li ++ "\n")
+                                    arvPrint dir
 
--- Input teste: arvPrint(amalgamate (sortLs (allNumWords (numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional"))))
+
+
+main = do putStr ("Insira o caminho relativo do arquivo: ")
+          hFlush stdout  -- Força o output a aparecer primeiro
+          file <- getLine
+          content <- readFile file
+  
+          putStr ("\nArquivo lido:")
+          putStr (show file) 
+  
+  -- RESPOSTA
+          putStr("\n\nDocumento indexado: ")
+          arvPrint(listToTree(allNumWords (numLines content)))
+
+-- Input teste: arvPrint(almalgamate (listToTree (allNumWords (numLines "Departamento de Ciencia da Computacao\nCurso de Ciencia da Computacao\nProgramacao Funcional"))))
